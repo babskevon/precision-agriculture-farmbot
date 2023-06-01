49ab1ea75320352ef4ab793d52499866a48b67aa
Kevin Micheal Babise <babskevon@gmail.com>
2023-06-01 13:53:43 +0300

    changed static

--- hybrid/settings.py
+++ hybrid/settings.py
@@ -143,12 +143,12 @@ USE_TZ = True
 import os
 STATIC_URL = "/static/"
 MEDIA_URL = '/media/'
-#MEDIA_ROOT = os.path.join(  BASE_DIR, "media")
+MEDIA_ROOT = os.path.join(  BASE_DIR, "media")
 # print(BASE_DIR)
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATIC_ROOT = '/root/static'
-MEDIA_ROOT = '/root/media'
+#STATIC_ROOT = '/root/static'
+#MEDIA_ROOT = '/root/media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
